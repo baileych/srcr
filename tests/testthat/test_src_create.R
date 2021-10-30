cdc52e528ece9b28ca3efea07845fa274a920479
Charles Bailey <baileyc@email.chop.edu>
2021-10-30 01:32:45 -0400

    Hush deprecation warning for src_sqlite()

--- tests/testthat/test_src_create.R
+++ tests/testthat/test_src_create.R
@@ -13,6 +13,7 @@ test.data <- data.frame(ids = c(1, 2, 3), strs = c('a', 'b', 'c'))
 
 test_that('Proper dplyr src is set up by srcr()', {
     has_sqlite()
+    withr::local_options(lifecycle_verbosity = "quiet")
     expect_is((mysrc <- get.src()), 'src_sql')
     expect_is((test.table <- copy_to(mysrc, test.data, name = 'test_table')),
               'tbl')
